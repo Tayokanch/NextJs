4c582b7ead8927a4c0d39685020f99374e904163
Tayokanch <tayoquadri11@gmail.com>
2024-07-16 19:13:39 +0100

    crud created

--- app/(API)/api/users/[id]/route.tsx
+++ app/(API)/api/users/[id]/route.tsx
@@ -1,39 +1,71 @@
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 interface Props {
   params: { id: number };
 }
 const GET = async (request: NextRequest, { params: { id } }: Props) => {
-  console.log("This is id:", id);
-  if (id > 10) {
-    return NextResponse.json(
-      { error: `The user with the id ${id} is not qualified` },
-      { status: 404 }
-    );
-  }
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
 
-  return NextResponse.json({ id: 1, name: "Tayo" });
+  return NextResponse.json({ user: foundUser }, { status: 200 });
 };
 
 const PUT = async (request: NextRequest, { params: { id } }: Props) => {
-  const body = await request.json();
-  if (!body.name) {
-    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  const { name, email } = await request.json();
+  if (!name || !email) {
+    return NextResponse.json(
+      { error: " all are fields are required" },
+      { status: 400 }
+    );
   }
 
-  if (id > 10) {
-    return NextResponse.json({ error: "user not found" }, { status: 400 });
-  }
+  const findUser = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
 
-  return NextResponse.json({ id: 1, name: "Yewande" }, { status: 200 });
+  if (!findUser) {
+    return NextResponse.json({
+      message: `user with the given id: ${id} not found`,
+    });
+  }
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: findUser.id,
+    },
+    data: {
+      name: name,
+      email: email,
+    },
+  });
+  return NextResponse.json({ message: updatedUser }, { status: 201 });
 };
 
 const DELETE = async (request: NextRequest, { params: { id } }: Props) => {
-  if (id > 10) {
-    return NextResponse.json({ error: "user not found" }, { status: 400 });
+  const CheckedUser = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!CheckedUser) {
+    return NextResponse.json({
+      message: `user with the give id: ${id} not found`,
+    });
   }
+
+  await prisma.user.delete({
+    where: {
+      id: CheckedUser.id,
+    },
+  });
   return NextResponse.json(
-    { message: "user has been deleted" },
+    { message: "Usee has been sucessfuly deleted" },
     { status: 200 }
   );
 };
--- app/(API)/api/users/route.tsx
+++ app/(API)/api/users/route.tsx
@@ -1,23 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
 import { UsernameSchema } from "./schema";
+import { Prisma } from "@prisma/client";
+import prisma from "@/prisma/client";
+
+interface Props {
+  params: { id: number };
+}
 const GET = async (request: NextRequest) => {
-  return NextResponse.json([
-    { id: 1, name: "Tayo" },
-    { id: 2, name: "Tope" },
-  ]);
+  const users = await prisma.user.findMany();
+  return NextResponse.json({ users }, { status: 200 });
 };
 
 const POST = async (request: NextRequest) => {
   const body = await request.json();
+
   const validation = UsernameSchema.safeParse(body);
+
   if (!validation.success) {
-    return NextResponse.json( validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  return NextResponse.json(
-    { id: 1, name: body.name, email: body.email },
-    { status: 201 }
-  );
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+  if (findUser) {
+    return NextResponse.json(
+      { message: "User with an email already exists" },
+      { status: 400 }
+    );
+  }
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 };
 
 export { GET, POST };
